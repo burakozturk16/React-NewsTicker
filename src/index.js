395fd46cc68608bcaa7da6dc055c8b13266f89ed
Burak Öztürk <burakozturk16@gmail.com>
2020-10-01 13:35:45 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Ticker from "./components/Ticker";
+
+class App extends React.Component {
+    finito(){
+        //alert("bitti lan");
+    };
+
+    render() {
+        return (
+            <div>
+                <Ticker
+                    mode="smooth" // chain, await, smooth
+                    speed={10}
+                    direction="toLeft"  // toLeft, toRight
+                    offset={0}
+                    height={50}
+                    onComplete={this.finito}
+                >
+                    {(index) => (
+                        <>
+                            <h1>Kayıyorum bir yıldız gibi..</h1>
+                        </>
+                    )}
+                </Ticker>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
